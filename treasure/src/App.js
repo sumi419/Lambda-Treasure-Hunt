7e5e10938cf67c3500413994e6c7f7ea0e09e781
sumi419 <sasgar04@gmail.com>
2019-02-12 12:39:43 -0800

    state updated 🍓

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -5,12 +5,37 @@ import axios from 'axios'
 import config from './secret'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      room: {
+      roomId: 0,
+      title: '',
+      coordinates: "",
+      exits: [],
+      cooldown: 0, //1000 setInterval
+      errors: '',
+      description: '',
+      elevation: 0,
+      items: [],
+      messages: [],
+      playrs: [],
+      terrain: ''
+      }
+    }
+  }
+
+ 
+  
   componentDidMount() {
     const headers = {headers : {Authorization: config.token}}
     axios
     .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers)
     .then(response => {
       console.log(response.data)
+      this.setState({
+        room: response.data
+      })
     })
     .catch(error => {
       console.log(error.response.data)
