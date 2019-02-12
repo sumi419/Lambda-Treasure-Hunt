738f391e1adead66ac18d7521f2c755b4d3e59ce
sumi419 <sasgar04@gmail.com>
2019-02-11 17:10:00 -0800

    token added 🎀

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import config from './secret'
 
 class App extends Component {
+  componentDidMount() {
+    const headers = {headers : {Authorization: config.token}}
+    axios
+    .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log(error.response.data)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Hi
       </div>
     );
   }
