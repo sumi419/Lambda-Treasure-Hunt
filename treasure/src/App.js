4c1285527807b800e40609612813552a305586f2
sumi419 <sasgar04@gmail.com>
2019-02-14 13:56:36 -0800

    updating handle movement 🐈

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -27,22 +27,12 @@ class App extends Component {
     };
   }
 
-  // creates exit obj
-  createExitObj() {
-    const coordinatesObj = {
-      x: this.state.coordinates.replace(/["()]/gi, '').split(',')[0],
-      y: this.state.coordinates.replace(/["()]/gi, '').split(',')[1]
-    };
-    const exitObj = {};
-    if (!this.graph[`Room ${this.state.room_id}`]) {
-      for (let exit of this.state.exits) {
-        exitObj[exit] = '?';
-      }
-      this.graph[`Room ${this.state.room_id}`] = [coordinatesObj, exitObj];
-    }
-    //value of graph at room id gives you (coordinates, exitObj)
-    return this.graph[`Room ${this.state.room_id}`];
-  }
+  getCoordinates = (coordinates) => {
+    const x = coordinates.replace(/[%^()]/g, '').split(',')[0];
+    const y = coordinates.replace(/[%^()]/g, '').split(',')[1];
+    const coordsObject = { x, y };
+    return coordsObject;
+  };
 
   inverseDirection = (direction) => {
     const inverseDir = { n: 's', s: 'n', w: 'e', e: 'w' };
@@ -73,32 +63,32 @@ class App extends Component {
     return graph;
   };
 
-  backtrack_to_unexplored(starting_vertex_id = this.state.room_id, target = '?') {
-    const queue = [];
-    queue.push([starting_vertex_id]);
-    const visited = [];
-    while (queue.length > 0) {
-      let dequeued = queue.shift();
-      let last_room = dequeued[dequeued.length - 1];
-      for (let exit in last_room) {
-        if (last_room[exit] === target) {
-          dequeued.pop();
-          return dequeued;
-        } else {
-          visited.add(last_room[exit]);
-          for (let path in this.graph[last_room[exit]].exits) {
-            if (visited.has(this.graph[last_room[exit]].exits[path]) === false) {
-              let path_copy = [...dequeued];
-              path_copy.push({ [path]: this.graph[last_room[exit]].exits[path] });
-              queue.push(path_copy);
-              console.log(queue);
-            }
-          }
-          console.log(queue);
-        }
-      }
-    }
-  }
+  // backtrack_to_unexplored(starting_vertex_id = this.state.room_id, target = '?') {
+  //   const queue = [];
+  //   queue.push([starting_vertex_id]);
+  //   const visited = [];
+  //   while (queue.length > 0) {
+  //     let dequeued = queue.shift();
+  //     let last_room = dequeued[dequeued.length - 1];
+  //     for (let exit in last_room) {
+  //       if (last_room[exit] === target) {
+  //         dequeued.pop();
+  //         return dequeued;
+  //       } else {
+  //         visited.add(last_room[exit]);
+  //         for (let path in this.graph[last_room[exit]].exits) {
+  //           if (visited.has(this.graph[last_room[exit]].exits[path]) === false) {
+  //             let path_copy = [...dequeued];
+  //             path_copy.push({ [path]: this.graph[last_room[exit]].exits[path] });
+  //             queue.push(path_copy);
+  //             console.log(queue);
+  //           }
+  //         }
+  //         console.log(queue);
+  //       }
+  //     }
+  //   }
+  // }
 
   // path_to_directions(path) {
   //   const current_room = path[0]
@@ -112,33 +102,6 @@ class App extends Component {
   //     }
   //   }
   // }
-  createGraph() {
-    console.log('in here');
-    const inverse_directions = {
-      n: 's',
-      s: 'n',
-      e: 'w',
-      w: 'e'
-    };
-    const currentRoomExits = this.state.exits;
-    console.log(currentRoomExits);
-    const unexploredExits = [];
-    // look for ?s to append to unexplored
-    for (let direction of currentRoomExits) {
-      if (direction === '?') {
-        unexploredExits.push(direction);
-      }
-      if (unexploredExits.length > 0) {
-        const firstExit = unexploredExits[0];
-        this.traversalPath.push(firstExit);
-        const prev_room_id = this.state.room_id;
-        this.handleMovement(direction);
-        this.graph[prev_room_id][firstExit] = this.state.room_id;
-        this.graph[this.state.room_id][inverse_directions[firstExit]] = prev_room_id;
-      }
-    }
-    localStorage.setItem('graph', JSON.stringify(this.graph));
-  }
 
   componentDidMount() {
     const headers = {
@@ -150,20 +113,6 @@ class App extends Component {
       const graph = JSON.parse(localStorage.getItem('graph'));
       this.graph = graph;
     }
-    // this.graph = {
-    //   10: [
-    //     {
-    //       x: 'x',
-    //       y: 'y'
-    //     },
-    //     {
-    //       n: '?',
-    //       e: '?',
-    //       s: '?',
-    //       w: '?'
-    //     }
-    //   ]
-    // };
     console.log(this.graph);
     axios
       .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers)
@@ -180,14 +129,20 @@ class App extends Component {
             exits: response.data.exits
           };
         });
+        const prev_room_id = this.state.room_id;
+        const graph = this.updateGraph(
+          this.state.room_id,
+          this.getCoordinates(this.state.coordinates),
+          this.state.exits,
+          prev_room_id
+        );
+        this.setState({ graph });
       })
       .catch((error) => {
         console.log(error.response.data);
       });
     setTimeout(() => {
       this.handleMovement();
-      this.createGraph();
-      this.createExitObj();
       console.log(this.state.exits);
     }, 500);
   }
@@ -215,6 +170,14 @@ class App extends Component {
             exits: response.data.exits
           };
         });
+        const prev_room_id = this.state.room_id;
+        const graph = this.updateGraph(
+          this.state.room_id,
+          this.getCoordinates(this.state.coordinates),
+          this.state.exits,
+          prev_room_id,
+          direction
+        );
       })
       .catch((error) => console.error(error.response.data));
   }
